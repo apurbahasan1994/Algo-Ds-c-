2d933a595e2385aa440e71ccdcd64f05861877c1
Apurba Hasan <apurbahasan1994@gmail.com>
2021-11-05 10:43:57 +0600

    added leetycode_35

---
+++ leelcode_35/35.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int binary_search(vector<int>&nums,int target,int s,int e)
+    {
+        if(s<=e)
+        {
+            int mid=(s+e)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            if(nums[mid]<target)
+            {
+                return this->binary_search(nums,target,mid+1,e);
+            }
+            return this->binary_search(nums,target,s,mid-1);
+        }
+        return s;
+    }
+    int searchInsert(vector<int>& nums, int target) {
+        int s=0;
+        int e=nums.size()-1;
+        int ans=this->binary_search(nums,target,s,e);
+        return ans;
+    }
+};
